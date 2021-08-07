d0d4a2c710a56be47604a6f28dc922c4d204ffb3
Yunosuke Sakai <qmkns246@gmail.com>
2021-08-08 01:03:54 +0900

    Done Chapter1

--- code-ch01/ecc.py
+++ code-ch01/ecc.py
@@ -22,8 +22,9 @@ class FieldElement:
     # end::source1[]
 
     def __ne__(self, other):
-        # this should be the inverse of the == operator
-        raise NotImplementedError
+        if other is None:
+            return True
+        return not(self.num == other.num and self.prime == other.prime)
 
     # tag::source2[]
     def __add__(self, other):
@@ -36,18 +37,14 @@ class FieldElement:
     def __sub__(self, other):
         if self.prime != other.prime:
             raise TypeError('Cannot subtract two numbers in different Fields')
-        # self.num and other.num are the actual values
-        # self.prime is what we need to mod against
-        # We return an element of the same class
-        raise NotImplementedError
+        num = (self.num - other.num) % self.prime
+        return self.__class__(num, self.prime)
 
     def __mul__(self, other):
         if self.prime != other.prime:
             raise TypeError('Cannot multiply two numbers in different Fields')
-        # self.num and other.num are the actual values
-        # self.prime is what we need to mod against
-        # We return an element of the same class
-        raise NotImplementedError
+        num = (self.num * other.num) % self.prime
+        return self.__class__(num, self.prime)
 
     # tag::source3[]
     def __pow__(self, exponent):
@@ -64,7 +61,8 @@ class FieldElement:
         # this means:
         # 1/n == pow(n, p-2, p)
         # We return an element of the same class
-        raise NotImplementedError
+        num = (self.num * pow(other.num, self.prime-2, self.prime)) % self.prime
+        return self.__class__(num, self.prime)
 
 
 class FieldElementTest(TestCase):
